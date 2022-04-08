9384a19e57de4daa3eace74ece5c73811ab777b1
weijiaduo <648620282@qq.com>
2022-04-09 06:37:28 +0800

    08-a-解析鼠标数据

--- src/bootpack.c
+++ src/bootpack.c
@@ -14,6 +14,8 @@ void HariMain(void)
 	char mcursor[256];
 	char keybuf[32];
 	char mousebuf[128];
+	unsigned char mouse_dbuf[3];
+	unsigned char mouse_phase;
 	int mx;
 	int my;
 	int i;
@@ -57,6 +59,7 @@ void HariMain(void)
 
 	/* 激活鼠标 */
 	enable_mouse();
+	mouse_phase = 0; /* 进入到等待鼠标的0xfa的状态 */
 
   for (;;)
   {
@@ -75,9 +78,38 @@ void HariMain(void)
 			/* 鼠标数据 */
 			i = fifo8_get(&mousefifo);
 			io_sti();
-			sprintf(s, "%02X", i);
-			boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-			putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+			
+			if (mouse_phase == 0)
+			{
+				/* 等待鼠标0xfa的状态 */
+				if (i == 0xfa)
+				{
+					mouse_phase = 1;
+				}
+			}
+			else if (mouse_phase == 1)
+			{
+				/* 等待鼠标的第一字节 */
+				mouse_dbuf[0] = i;
+				mouse_phase = 2;
+			}
+			else if (mouse_phase == 2)
+			{
+				/* 等待鼠标的第二字节 */
+				mouse_dbuf[1] = i;
+				mouse_phase = 3;
+			}
+			else if (mouse_phase == 3)
+			{
+				/* 等待鼠标的第三字节 */
+				mouse_dbuf[2] = i;
+				mouse_phase = 1;
+
+				/* 鼠标的3个字节都齐了，显示出来 */
+				sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
+				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+			}
 		}
 		else
 		{
