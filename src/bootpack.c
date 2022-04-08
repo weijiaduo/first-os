c0ce96100904091a8d884e233d30145f7c90ac99
weijiaduo <648620282@qq.com>
2022-04-09 06:27:02 +0800

    06-c

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,82 +1,9 @@
+#include "bootpack.h"
 #include <stdio.h>
-
-/* 声明函数，函数定义在别的文件里 */
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-/* 调色板 */
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-
-/* 绘制 */
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen8(char *vram, int x, int y);
-
-/* 打印 */
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-
-/* 鼠标 */
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
-
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
-
-struct BOOTINFO {
-	char cyls;
-	char leds;
-	char vmode;
-	char reserve;
-	short scrnx;
-	short scrny;
-	char *vram;
-};
-
-struct SEGMENT_DESCRIPTOR {
-	short limit_low;
-	short base_low;
-	char base_mid;
-	char access_right;
-	char limit_high;
-	char base_high;
-};
-
-struct GATE_DESCRIPTOR {
-	short offset_low;
-	short selector;
-	char dw_count;
-	char access_right;
-	short offset_high;
-};
-
-/* 段表、中断记录表 */
-void init_gdtidt(void);
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
 
 void HariMain(void)
 {
-	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
 	char s[40];
 	char mcursor[256];
 	int mx;
--- src/graphic.c
+++ src/graphic.c
@@ -1,42 +1,4 @@
-/* 声明函数，函数定义在别的文件里 */
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-
-/* 调色板 */
-void init_palette(void);
-void set_palette(int start, int end, unsigned char *rgb);
-
-/* 绘制 */
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void init_screen8(char *vram, int x, int y);
-
-/* 打印 */
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-
-/* 鼠标 */
-void init_mouse_cursor8(char *mouse, char bc);
-void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
-
-#define COL8_000000		0
-#define COL8_FF0000		1
-#define COL8_00FF00		2
-#define COL8_FFFF00		3
-#define COL8_0000FF		4
-#define COL8_FF00FF		5
-#define COL8_00FFFF		6
-#define COL8_FFFFFF		7
-#define COL8_C6C6C6		8
-#define COL8_840000		9
-#define COL8_008400		10
-#define COL8_848400		11
-#define COL8_000084		12
-#define COL8_840084		13
-#define COL8_008484		14
-#define COL8_848484		15
+#include "bootpack.h"
 
 void init_palette(void)
 {
@@ -117,6 +79,7 @@ void init_screen8(char *vram, int x, int y)
 	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
 	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
 	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+	return;
 }
 
 /**
