6cc6837bf8367aa9a9a4b2d7bc110486cf95cd7d
weijiaduo <648620282@qq.com>
2022-04-09 06:38:19 +0800

    08-b-封装鼠标数据解析函数

--- src/bootpack.c
+++ src/bootpack.c
@@ -1,11 +1,16 @@
 #include "bootpack.h"
 #include <stdio.h>
 
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+};
+
 extern struct FIFO8 keyfifo;
 extern struct FIFO8 mousefifo;
 
-void enable_mouse(void);
 void init_keyboard(void);
+void enable_mouse(struct MOUSE_DEC *mdec);
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
 
 void HariMain(void)
 {
@@ -14,8 +19,7 @@ void HariMain(void)
 	char mcursor[256];
 	char keybuf[32];
 	char mousebuf[128];
-	unsigned char mouse_dbuf[3];
-	unsigned char mouse_phase;
+	struct MOUSE_DEC mdec;
 	int mx;
 	int my;
 	int i;
@@ -58,8 +62,7 @@ void HariMain(void)
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
 	/* 激活鼠标 */
-	enable_mouse();
-	mouse_phase = 0; /* 进入到等待鼠标的0xfa的状态 */
+	enable_mouse(&mdec);
 
   for (;;)
   {
@@ -78,35 +81,11 @@ void HariMain(void)
 			/* 鼠标数据 */
 			i = fifo8_get(&mousefifo);
 			io_sti();
-			
-			if (mouse_phase == 0)
-			{
-				/* 等待鼠标0xfa的状态 */
-				if (i == 0xfa)
-				{
-					mouse_phase = 1;
-				}
-			}
-			else if (mouse_phase == 1)
-			{
-				/* 等待鼠标的第一字节 */
-				mouse_dbuf[0] = i;
-				mouse_phase = 2;
-			}
-			else if (mouse_phase == 2)
-			{
-				/* 等待鼠标的第二字节 */
-				mouse_dbuf[1] = i;
-				mouse_phase = 3;
-			}
-			else if (mouse_phase == 3)
-			{
-				/* 等待鼠标的第三字节 */
-				mouse_dbuf[2] = i;
-				mouse_phase = 1;
 
+			if (mouse_decode(&mdec, i) != 0)
+			{
 				/* 鼠标的3个字节都齐了，显示出来 */
-				sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+				sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
 				boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 32 + 8 * 8 - 1, 31);
 				putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
 			}
@@ -152,11 +131,51 @@ void init_keyboard(void)
 #define MOUSECMD_ENABLE			0xf4
 
 /** 激活鼠标 */
-void enable_mouse(void)
+void enable_mouse(struct MOUSE_DEC *mdec)
 {
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	return; /* 顺利的话，键盘控制器会返送ACK（0xfa） */
+
+	/* 顺利的话，键盘控制器会返送ACK（0xfa） */
+	mdec->phase = 0;
+	return;
+}
+
+/** 鼠标数据解码 */
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
+	if (mdec->phase == 0)
+	{
+		/* 等待鼠标0xfa的状态 */
+		if (dat == 0xfa)
+		{
+			mdec->phase = 1;
+		}
+		return 0;
+	}
+	if (mdec->phase == 1)
+	{
+		/* 等待鼠标的第一字节 */
+		mdec->buf[0] = dat;
+		mdec->phase = 2;
+		return 0;
+	}
+	if (mdec->phase == 2)
+	{
+		/* 等待鼠标的第二字节 */
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	}
+	if (mdec->phase == 3)
+	{
+		/* 等待鼠标的第三字节 */
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		return 1;
+	}
+	/* 应该不可能到这里来 */
+	return -1;
 }
