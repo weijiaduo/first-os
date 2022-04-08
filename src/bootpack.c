dc4ce684e0151d160e9271582286b1babb21a00b
weijiaduo <648620282@qq.com>
2022-04-09 06:23:00 +0800

    05-f

--- src/bootpack.c
+++ src/bootpack.c
@@ -12,6 +12,7 @@ void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
 void init_screen(char *vram, int x, int y);
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
 
 #define COL8_000000		0
 #define COL8_FF0000		1
@@ -43,7 +44,6 @@ struct BOOTINFO {
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
-	extern char hankaku[4096];
 
 	/* 设定调色板 */
   init_palette();
@@ -52,12 +52,9 @@ void HariMain(void)
   init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
 
 	/* 输出字符 */
-	putfont8(binfo->vram, binfo->scrnx,  8, 8, COL8_FFFFFF, hankaku + 'A' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 16, 8, COL8_FFFFFF, hankaku + 'B' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 24, 8, COL8_FFFFFF, hankaku + 'C' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 40, 8, COL8_FFFFFF, hankaku + '1' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 48, 8, COL8_FFFFFF, hankaku + '2' * 16);
-	putfont8(binfo->vram, binfo->scrnx, 56, 8, COL8_FFFFFF, hankaku + '3' * 16);
+	putfonts8_asc(binfo->vram, binfo->scrnx,  8,  8, COL8_FFFFFF, "ABC 123");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 31, 31, COL8_000000, "Haribote OS.");
+	putfonts8_asc(binfo->vram, binfo->scrnx, 30, 30, COL8_FFFFFF, "Haribote OS.");
 
   for (;;)
   {
@@ -110,6 +107,9 @@ void set_palette(int start, int end, unsigned char *rgb)
   return;
 }
 
+/**
+ * 绘制指定的区域范围
+ */ 
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1)
 {
 	int x, y;
@@ -143,6 +143,9 @@ void init_screen(char *vram, int x, int y)
 	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
 }
 
+/**
+ * 打印指定的字符
+ */
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 {
 	int i;
@@ -160,6 +163,20 @@ void putfont8(char *vram, int xsize, int x, int y, char c, char *font)
 		if ((d & 0x04) != 0) { p[5] = c; }
 		if ((d & 0x02) != 0) { p[6] = c; }
 		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
+/**
+ * 按ASCII码打印字符串
+ */
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
+{
+	extern char hankaku[4096];
+	for (; *s != 0x00; s++)
+	{
+		putfont8(vram, xsize, x, y, c, hankaku + (*s) * 16);
+		x += 8;
 	}
 	return;
 }
