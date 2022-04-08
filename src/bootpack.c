fd92322e9a86f9e228fa6303a5d816fd76898938
weijiaduo <648620282@qq.com>
2022-04-09 06:36:08 +0800

    07-f-激活鼠标电路

--- src/bootpack.c
+++ src/bootpack.c
@@ -3,6 +3,9 @@
 
 extern struct FIFO8 keyfifo;
 
+void enable_mouse(void);
+void init_keyboard(void);
+
 void HariMain(void)
 {
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
@@ -26,6 +29,13 @@ void HariMain(void)
 	/* 初始化键盘输入缓冲区 */
 	fifo8_init(&keyfifo, 32, keybuf);
 
+	/* 启用PIC */
+	io_out8(PIC0_IMR, 0xf9); /* 11111001 启用IRQ1（键盘）和IRQ2 */
+	io_out8(PIC1_IMR, 0xef); /* 11101111 启用IRQ12（鼠标） */
+
+	/* 初始化键盘控制电路 */
+	init_keyboard();
+
 	/* 初始化调色板 */
   init_palette();
   
@@ -42,9 +52,8 @@ void HariMain(void)
 	sprintf(s, "(%d, %d)", mx, my);
 	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-	/* 启用PIC */
-	io_out8(PIC0_IMR, 0xf9); /* 11111001 启用IRQ1（键盘）和IRQ2 */
-	io_out8(PIC1_IMR, 0xef); /* 11101111 启用IRQ12（鼠标） */
+	/* 激活鼠标 */
+	enable_mouse();
 
   for (;;)
   {
@@ -63,4 +72,47 @@ void HariMain(void)
 		}
     io_hlt();
   }
+}
+
+#define PORT_KEYDAT				0x0060
+#define PORT_KEYSTA				0x0064
+#define PORT_KEYCMD				0x0064
+#define KEYSTA_SEND_NOTREADY	0x02
+#define KEYCMD_WRITE_MODE		0x60
+#define KBC_MODE				0x47
+
+/** 等待键盘控制电路准备完毕 */
+void wait_KBC_sendready(void)
+{
+	for (;;)
+	{
+		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0)
+		{
+			break;
+		}
+	}
+	return;
+}
+
+/** 初始化键盘 */
+void init_keyboard(void)
+{
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, KBC_MODE);
+	return;
+}
+
+#define KEYCMD_SENDTO_MOUSE		0xd4
+#define MOUSECMD_ENABLE			0xf4
+
+/** 激活鼠标 */
+void enable_mouse(void)
+{
+	wait_KBC_sendready();
+	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
+	wait_KBC_sendready();
+	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
+	return; /* 顺利的话，键盘控制器会返送ACK（0xfa） */
 }
