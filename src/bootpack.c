b80c8074a137ce56d1126ecbdc92cd08a44d1efd
weijiaduo <648620282@qq.com>
2022-05-01 21:21:21 +0800

    17-c-切换输入窗口

--- src/bootpack.c
+++ src/bootpack.c
@@ -3,6 +3,7 @@
 #include "bootpack.h"
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act);
 void putfonts_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
 void make_textbox8(struct SHEET *sht, int x0, int y0, int sx, int sy, int c);
 
@@ -47,6 +48,7 @@ void HariMain(void)
 
 	int mx, my, i;
 	int cursor_x, cursor_c;
+	int key_to = 0; /* 键盘字符输出的位置 */
 
 	/* 初始化段表和中断记录表 */
 	init_gdtidt();
@@ -188,6 +190,24 @@ void HariMain(void)
 						putfonts_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
 						cursor_x -= 8;
 					}
+					if (i == 256 + 0x0f)
+					{
+						/* Tab 键 */
+						if (key_to == 0)
+						{
+							key_to = 1;
+							make_wtitle8(buf_win, sht_win->bxsize, "task_a", 0);
+							make_wtitle8(buf_cons, sht_cons->bxsize, "console", 1);
+						}
+						else
+						{
+							key_to = 0;
+							make_wtitle8(buf_win, sht_win->bxsize, "task_a", 1);
+							make_wtitle8(buf_cons, sht_cons->bxsize, "console", 0);
+						}
+						sheet_refresh(sht_win, 0, 0, sht_win->bxsize, 21);
+						sheet_refresh(sht_cons, 0, 0, sht_cons->bxsize, 21);
+					}
 					/* 光标显示 */
 					boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
 					sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
@@ -269,6 +289,22 @@ void HariMain(void)
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act)
 {
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         xsize - 1, 0        );
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         xsize - 2, 1        );
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         0,         ysize - 1);
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         1,         ysize - 2);
+	boxfill8(buf, xsize, COL8_848484, xsize - 2, 1,         xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, xsize - 1, 0,         xsize - 1, ysize - 1);
+	boxfill8(buf, xsize, COL8_C6C6C6, 2,         2,         xsize - 3, ysize - 3);
+	boxfill8(buf, xsize, COL8_848484, 1,         ysize - 2, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, 0,         ysize - 1, xsize - 1, ysize - 1);
+
+	make_wtitle8(buf, xsize, title, act);
+	return;
+}
+
+void make_wtitle8(unsigned char *buf, int xsize, char *title, char act)
+{
 	static char closebtn[14][16] = {
 		"OOOOOOOOOOOOOOO@",
 		"OQQQQQQQQQQQQQ$@",
@@ -283,7 +319,8 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
 		"OQQQQQQQQQQQQQ$@",
 		"OQQQQQQQQQQQQQ$@",
 		"O$$$$$$$$$$$$$$@",
-		"@@@@@@@@@@@@@@@@"};
+		"@@@@@@@@@@@@@@@@"
+	};
 
 	int x, y;
 	char c;
@@ -298,17 +335,8 @@ void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char ac
 		tc = COL8_C6C6C6;
 		tbc = COL8_848484;
 	}
-	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         xsize - 1, 0        );
-	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         xsize - 2, 1        );
-	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         0,         ysize - 1);
-	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         1,         ysize - 2);
-	boxfill8(buf, xsize, COL8_848484, xsize - 2, 1,         xsize - 2, ysize - 2);
-	boxfill8(buf, xsize, COL8_000000, xsize - 1, 0,         xsize - 1, ysize - 1);
-	boxfill8(buf, xsize, COL8_C6C6C6, 2,         2,         xsize - 3, ysize - 3);
-	boxfill8(buf, xsize, tbc,         3,         3,         xsize - 4, 20       );
-	boxfill8(buf, xsize, COL8_848484, 1,         ysize - 2, xsize - 2, ysize - 2);
-	boxfill8(buf, xsize, COL8_000000, 0,         ysize - 1, xsize - 1, ysize - 1);
 
+	boxfill8(buf, xsize, tbc, 3, 3, xsize - 4, 20);
 	putfonts8_asc(buf, xsize, 24, 4, tc, title);
 	for (y = 0; y < 14; y++)
 	{
