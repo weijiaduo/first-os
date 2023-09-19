03bef6686f255f7f011e934e95791278cf9bba2c
weijiaduo <648620282@qq.com>
2023-09-20 03:39:10 +0800

    28-a-alloca栈空间测试

---
+++ src/sosu3/sosu3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "apilib.h"
+
+#define MAX		10000
+
+void HariMain(void)
+{
+    char *flag, s[8];
+    int i, j;
+    api_initmalloc();
+    flag = api_malloc(MAX);
+    for (i = 0; i < MAX; i++)
+    {
+        flag[i] = 0;
+    }
+    for (i = 2; i < MAX; i++)
+    {
+        if (flag[i] == 0)
+        {
+            /* 没有标记的是质数 */
+            sprintf(s, "%d ", i);
+            api_putstr0(s);
+            for (j = i * 2; j < MAX; j += i)
+            {
+                /* 给它的倍数做上标记 */
+                flag[j] = 1;
+            }
+        }
+    }
+    api_end();
+}
